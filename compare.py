fa8c08fd71e7fcfaf4b76c87b75187e042a3e1db
Hideo Daikoku <t16559hd@sfc.keio.ac.jp>
2019-01-09 13:31:41 +0900

    second commit

---
+++ compare.py
@@ -0,0 +1,46 @@
+from nltk import agreement
+from mpl_toolkits.mplot3d import Axes3D 
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+users # user data as stored in a numpy array
+
+# read data from csv
+def read_data(files):
+    
+    # converts files to numpy arrays 
+
+def stylistic_aesthetic():
+    
+    #returns stylsitic to aesthetic comparisons for all three participants as a dictionary
+    return stylistic_aesthetic_output
+
+def aesthetic():
+
+    #returns aesthetic to aesthetic for all three participants as a dictionary
+    return aesthetic_output
+
+def stylistic():
+
+    #returns aestetic 
+    return stylsitic_output
+
+def to_csv(filename):
+
+    #converts outputs ot csv and saves the filename
+
+if __name__ == '__main__':
+    for file in songwise:
+        files = list(file)
+    
+    read_data(files)
+    
+    output_1 = stylistic_aesthetic()
+    to_csv(output_1)
+
+    output_2 = stylistic()
+    to_csv(output_1)
+    
+    output_3 = aesthetic()
+    to_csv(output_1)
