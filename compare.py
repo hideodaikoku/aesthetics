134399f5a9305ab52120443656e001fecbfd381a
Hideo Daikoku <neildaikoku@gmail.com>
2019-01-14 17:02:06 +0900

    Added Visualizations

--- compare.py
+++ compare.py
@@ -1,4 +1,5 @@
 # from nltk import agreement
+import seaborn as sn
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,8 +8,8 @@ import os
 import errno
 
 # files = ['data/songwise/okazaki_SongWise.csv','data/songwise/akira_SongWise.csv', 'data/songwise/satoru_SongWise.csv']
-files = ['data/preference/okazaki_SongComparison_1220.csv','data/preference/akira_SongComparison_1223.csv', 'data/preference/satoru_SongComparison_1219.csv']
-# files = ['data/similarity/okazaki_SongComparison_1220.csv','data/similarity/akira_SongComparison_1223.csv', 'data/similarity/satoru_SongComparison_1219.csv']
+# files = ['data/preference/okazaki_SongComparison_1220.csv','data/preference/akira_SongComparison_1223.csv', 'data/preference/satoru_SongComparison_1219.csv']
+files = ['data/similarity/okazaki_SongComparison_1220.csv','data/similarity/akira_SongComparison_1223.csv', 'data/similarity/satoru_SongComparison_1219.csv']
 
 # file = 'data/similarity/satoru_SongComparison_1219.csv'
 # file = 'data/preference/satoru_SongComparison_1219.csv'
@@ -58,10 +59,9 @@ def feature_wise(files):
         path='output/songwise_'+str(i)+'.csv'
         
         f= open(path,"w+")
-        
         cor.to_csv(path_or_buf=path)
-        
         f.close()
+
     return users              
 
 def preference(files):
@@ -73,18 +73,28 @@ def preference(files):
 
     df_concat = pd.concat((users[0],users[1],users[2])).groupby(level=0)
     df_mean= df_concat.mean()
-    filename = "output/preference.csv"
+    df_mean.columns=['Central Africa', 'Bali','Java','Korea','Spain','Japan','Cuba','Central Asia', 'Central America','Australia','Brazil','Jazz', 'Ballad','Bulgaria','South India','Chinea','Europe','Chamber Music','North India','Boston']
+    df_mean.index=['Central Africa', 'Bali','Java','Korea','Spain','Japan','Cuba','Central Asia', 'Central America','Australia','Brazil','Jazz', 'Ballad','Bulgaria','South India','Chinea','Europe','Chamber Music','North India','Boston']
+    df_mean= df_mean.round(2)
     
-    if not os.path.exists(os.path.dirname(filename)):
-        try:
-            os.makedirs(os.path.dirname(filename))
-        except OSError as exc: # Guard against race condition
-            if exc.errno != errno.EEXIST:
-                raise
+    plt.figure(figsize = (10,6))
+    sn.heatmap(df_mean, annot=True, vmin=0, vmax=100)
+    plt.xticks(rotation=45, fontsize=6, ha='right')
+    plt.yticks(fontsize=6,va='center')
+    plt.title('Average User Preference Ratings',fontsize=16)
+    plt.show()
 
-    f= open(filename,"w+")
-    df_mean.to_csv(path_or_buf=filename)    
-    f.close()
+    # filename = "output/preference.csv"
+    # if not os.path.exists(os.path.dirname(filename)):
+    #     try:
+    #         os.makedirs(os.path.dirname(filename))
+    #     except OSError as exc: # Guard against race condition
+    #         if exc.errno != errno.EEXIST:
+    #             raise
+
+    # f= open(filename,"w+")
+    # df_mean.to_csv(path_or_buf=filename)    
+    # f.close()
     
     return users
 
@@ -96,23 +106,35 @@ def similarity(files):
         users[i]= users[i].drop(['Row'],axis=1)
 
     df_concat = pd.concat((users[0],users[1],users[2])).groupby(level=0)
-    df_mean= df_concat.mean()
-    filename = "output/similarity.csv"
     
-    if not os.path.exists(os.path.dirname(filename)):
-        try:
-            os.makedirs(os.path.dirname(filename))
-        except OSError as exc: # Guard against race condition
-            if exc.errno != errno.EEXIST:
-                raise
+    df_mean= df_concat.mean()
+    df_mean.columns=['Central Africa', 'Bali','Java','Korea','Spain','Japan','Cuba','Central Asia', 'Central America','Australia','Brazil','Jazz', 'Ballad','Bulgaria','South India','Chinea','Europe','Chamber Music','North India','Boston']
+    df_mean.index=['Central Africa', 'Bali','Java','Korea','Spain','Japan','Cuba','Central Asia', 'Central America','Australia','Brazil','Jazz', 'Ballad','Bulgaria','South India','Chinea','Europe','Chamber Music','North India','Boston']
+    df_mean= df_mean.round(2)
+    
+    plt.figure(figsize = (10,6))
+    sn.heatmap(df_mean, annot=True, vmin=0, vmax=100)
+    plt.xticks(rotation=45, fontsize=6, ha='right')
+    plt.yticks(fontsize=6,va='center')
+    plt.title('Average User Similarity Ratings',fontsize=16)
+    plt.show()
 
-    f= open(filename,"w+")
-    df_mean.to_csv(path_or_buf=filename)    
-    f.close()
+    # filename = "output/similarity.csv"
+    
+    # if not os.path.exists(os.path.dirname(filename)):
+    #     try:
+    #         os.makedirs(os.path.dirname(filename))
+    #     except OSError as exc: # Guard against race condition
+    #         if exc.errno != errno.EEXIST:
+    #             raise
+
+    # f= open(filename,"w+")
+    # df_mean.to_csv(path_or_buf=filename)    
+    # f.close()
 
     return users
 
 # format(file)
-# similarity(files)
-preference(files)
+similarity(files)
+# preference(files)
 # feature_wise(files)
